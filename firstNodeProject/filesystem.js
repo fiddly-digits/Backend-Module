5561f02d8d9d245d13b4bccd3b7fba987d4886fd
fiddly-digits <rob.crlu@gmail.com>
2023-05-31 21:18:33 -0600

    Testing Filesystem append

--- firstNodeProject/filesystem.js
+++ firstNodeProject/filesystem.js
@@ -16,3 +16,18 @@ fs.writeFile(
     console.log('File Saved Successfully');
   }
 );
+
+// * Creo que tiene prioridad el append, lo hace primero que el write.
+fs.appendFile(
+  'message.txt',
+  'And this is me testing append to a File',
+  (err) => {
+    if (err) throw err;
+    console.log('The String was appended to a file succesfully');
+  }
+);
+
+fs.appendFile('hola.txt', '\n Testing in another file', (err) => {
+  if (err) throw err;
+  console.log('The String was appended to a file succesfully');
+});
