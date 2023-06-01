619e1dde7b1003d66e4ff56249db23b791748189
fiddly-digits <rob.crlu@gmail.com>
2023-05-31 21:36:52 -0600

    Testing Filesystem unlink, mkdir, copy

--- firstNodeProject/filesystem.js
+++ firstNodeProject/filesystem.js
@@ -7,27 +7,50 @@ console.log('readfile', fs.readFile);
 //   console.log(data);
 // });
 
-fs.writeFile(
-  'message.txt',
-  'This is my first Node.js File created from codeeee',
-  'utf8',
-  (err) => {
-    if (err) throw err;
-    console.log('File Saved Successfully');
-  }
-);
+// ! Las funciones son Asincronas
+// * Crea un archivo
+// fs.writeFile(
+//   'message.txt',
+//   'This is my first Node.js File created from codeeee',
+//   'utf8',
+//   (err) => {
+//     if (err) throw err;
+//     console.log('File Saved Successfully');
+//   }
+// );
 
-// * Creo que tiene prioridad el append, lo hace primero que el write.
-fs.appendFile(
-  'message.txt',
-  'And this is me testing append to a File',
-  (err) => {
-    if (err) throw err;
-    console.log('The String was appended to a file succesfully');
-  }
-);
+// * Agrega texto a un archivo
+// fs.appendFile(
+//   'message.txt',
+//   'And this is me testing append to a File',
+//   (err) => {
+//     if (err) throw err;
+//     console.log('The String was appended to a file succesfully');
+//   }
+// );
 
-fs.appendFile('hola.txt', '\n Testing in another file', (err) => {
+// fs.appendFile('hola.txt', '\n Testing in another file', (err) => {
+//   if (err) throw err;
+//   console.log('The String was appended to a file succesfully');
+// });
+
+// * Elimina un archivo
+fs.unlink('hola.txt', (err) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log('The File was deleted.');
+});
+
+//*Copia Un Archivo
+fs.copyFile('message.txt', 'messageCopy.txt', (err) => {
   if (err) throw err;
-  console.log('The String was appended to a file succesfully');
+  console.log('The Copy was made succesfully');
+});
+
+// * Crea un directorio
+
+fs.mkdir('./newDir', (err) => {
+  if (err) throw err;
+  console.log('Se ha creado el directorio');
 });
